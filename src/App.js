6d5027541366890d010cc1b2ff1a3ce7fd939ed8
7prashant27 <prashantmarshall@gmail.com>
2024-04-02 23:45:53 +0530

    Deleted duplicate files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import MapContainer from './components/MapComponent';
-import MapContainer2 from './components/Map2';
 
 function App() {
   return (
     <div className="App">
      <MapContainer/>
-     {/* <MapContainer2/> */}
     </div>
   );
 }
